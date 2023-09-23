37c625bf60e4d10bf6085bb24b77cca9f81f01d1
Sudhir-Nishad <sudhirnishad@271gmail.com>
2023-09-23 14:27:54 +0530

    fixed

--- data/database.js
+++ data/database.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 export const connectDB = () => {
-    mongoose.connect("mongoURI", {
+    mongoose.connect("mongodb+srv://Sudhir-Nishad:sudhir123@cluster0.ybyy5vm.mongodb.net/?retryWrites=true", {
+        useNewUrlParser: true,
+        useUnifiedTopology:true,
     dbname:"backendAPI"
 })
     .then(() => {
